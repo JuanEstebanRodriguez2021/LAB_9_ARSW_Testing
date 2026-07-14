7b26bfe47d4ddb8d7ae544ee67513a7027dc5b16
unknown <juan.rodriguez-ce@labinfo.is.escuelaing.edu.co>
2026-07-14 12:20:26 -0500

    Primeros pasos

---
+++ src/main/java/edu/eci/arsw/testing/repository/OrderRepository.java
@@ -0,0 +1,5 @@
+package edu.eci.arsw.testing.repository;
+import edu.eci.arsw.testing.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface OrderRepository extends JpaRepository<Order, String> {
+}
